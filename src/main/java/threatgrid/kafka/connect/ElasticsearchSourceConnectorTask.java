927c53a7bd9a03312ba3facc31bf4010aa421e4f
Kirill Chernyshov <kchernys@cisco.com>
2023-10-17 11:37:13 +0200

    Add release action

--- src/main/java/threatgrid/kafka/connect/ElasticsearchSourceConnectorTask.java
+++ src/main/java/threatgrid/kafka/connect/ElasticsearchSourceConnectorTask.java
@@ -4,13 +4,11 @@ import java.io.Reader;
 import java.io.StringReader;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.stream.Collectors;
 
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
